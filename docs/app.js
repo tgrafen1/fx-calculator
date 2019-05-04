1a6c5446811658ce80634c83c5c86cd67fbdd8c8
Thomas Grafenau <1810738607@stud.fh-kufstein.ac.at>
2019-05-04 13:28:15 +0200

    svg new

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,5 @@
+const url = 'https://tgrafen1.github.io/fx-calculator/rates.json'
+
 document.querySelectorAll('select').forEach(element => {
     element.innerHTML = `
         <option value="EUR">EUR</option>
@@ -5,4 +7,43 @@ document.querySelectorAll('select').forEach(element => {
         <option value="CHF">CHF</option>
         <option value="SEK">SEK</option>
     `;
+});
+
+document.querySelector('button').addEventListener('click', () =>{
+    const inputValue = document.querySelector('[name="input-value"]').value;
+    const inputCurrency = document.querySelector('[name="input-currency"]').value;
+    const outputCurrency = document.querySelector('[name="output-currency"]').value;
+
+    convert(inputValue, inputCurrency, outputCurrency)
+        .then((outputValue) => {
+            document.querySelector('[name="output-value"]').value=outputValue;
+        });
 })
+
+function convert(inputValue, inputCurrency, outputCurrency) {
+
+    //const url = 'https://data.fixer.io/api/latest?base=EUR&symbols=USD,SEK,CHF&access_key=';
+
+    return new Promise((resolve, reject) => {
+
+        fetch(url).then(response => {
+            if (response.status == 200) {
+                return response.json().rates;
+            } else {
+                return Promise.reject('Failed to download rates');
+            }
+        }).then((rates) => {
+
+            rates["EUR"] = 1;
+
+            if (inputCurrency != "EUR") {
+                inputValue = inputValue / rates[inputCurrency];
+            }
+
+            resolve(inputValue * rates[outputCurrency]);
+        });
+
+    });
+
+
+}
